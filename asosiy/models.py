54811b9acc3749262b2e9bb16d878127d1ed60b1
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-12-28 21:31:46 -0800

    2

---
+++ asosiy/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Suv(models.Model):
+    brend = models.CharField(max_length=30)
+    narx = models.PositiveSmallIntegerField()
+    litr = models.PositiveSmallIntegerField()
+    batafsil = models.TextField()
+
+    def __str__(self):
+        return self.brend
+
+
+class Mijoz(models.Model):
+    ism = models.CharField(max_length=30)
+    tel = models.CharField(max_length=20)
+    manzil = models.CharField(max_length=50)
+    qarz = models.PositiveSmallIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ism
+
+
+class Admin(models.Model):
+    ism = models.CharField(max_length=30)
+    yosh = models.PositiveSmallIntegerField()
+    ish_vaqti = models.CharField(max_length=5)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ism
+
+
+class Haydovchi(models.Model):
+    ism = models.CharField(max_length=30)
+    tel = models.CharField(max_length=20)
+    kiritilgan_sana = models.DateField()
+
+
+class Buyurtma(models.Model):
+    suv = models.ForeignKey(Suv, on_delete=models.CASCADE)
+    sana = models.DateField(auto_now_add=True)
+    mijoz = models.ForeignKey(Mijoz, on_delete=models.CASCADE)
+    miqdor = models.PositiveSmallIntegerField()
+    umumiy_narx = models.PositiveIntegerField(default=0)
+    admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
+    haydovchi = models.ForeignKey(Haydovchi, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.sana

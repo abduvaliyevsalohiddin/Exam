a61e9a6f0acaf4dda6be8787093dfd2c8daf63b6
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-12-28 22:23:41 -0800

    7

--- Exam/urls.py
+++ Exam/urls.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from django.urls import path
 from asosiy.views import *
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Imtihoz uchun yozilgan API",
+        default_version="V1",
+        description="Test",
+        contact=openapi.Contact("Abduvaliyev Salohiddin. Email: abduvaliyevsalohiddin568@gmail.com")
+    ),
+    public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('suvlar/', SuvlarAPi.as_view()),
@@ -14,5 +27,7 @@ urlpatterns = [
     path('mijoz/<int:pk>/', MijozAPi.as_view()),
     path('admin/<int:pk>/', AdminAPi.as_view()),
     path('haydovchi/<int:pk>/', HaydovchiAPi.as_view()),
+
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0)),
 
 ]
